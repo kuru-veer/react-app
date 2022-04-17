045edc59d7bdfcf600cadf85ff481a154c05c088
rudhra-kuruvella <rudhrak@workathon.us>
2022-04-17 19:52:19 +0530

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <center>
+        <h1>
+          React App!!!
+        </h1>
+      </center>
+    </>
+  );
+};
+
+export default App;
